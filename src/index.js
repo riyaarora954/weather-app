8a5635a0493ef2be62c26de9c68cd4fa1c88f730
Riya Arora <arorariya954@gmail.com>
2024-06-17 16:25:53 +0530

    Revert "Added basic JS to the search engine"
    This reverts commit b805ccf33df600b34a6b6aeb025b81d754aa90e7.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function displayCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", displayCity);
